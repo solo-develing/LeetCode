3d6dfdaefb84a9a7a264ece85e9106ee228fd241
sahilsajan <sahilsajan1@gmail.com>
2025-12-30 17:41:03 -0400

    changes to q9

--- Easy/9.java
+++ Easy/9.java
@@ -1,11 +1,12 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x < 0){
+        // edge case for when number is negative it will always be false
+        if (x < 0) {
             return false;
         }
         int temp = x;
         int reverse = 0;
-        while(temp != 0){
+        while (temp != 0) {
             int last = temp % 10;
             reverse *= 10;
             reverse += last;
